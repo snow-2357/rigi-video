ba7c647b3090e5bbbf05aea2849d6d2720aacad3
simantaray <simantaray89@gmail.com>
2024-02-17 15:14:16 +0530

    toggle autoplay

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -7,6 +7,7 @@ function Video({ video, autoPlay }) {
   const [duration, setDuration] = useState(0);
   const [playbackSpeed, setPlaybackSpeed] = useState(1);
   const [waitNotification, setWaitNotification] = useState(false);
+  const [autoplaySwitch, setAutoplaySwitch] = useState(true);
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -68,7 +69,12 @@ function Video({ video, autoPlay }) {
   };
 
   useEffect(() => {
-    if (currentTime !== 0 && duration !== 0 && currentTime === duration) {
+    if (
+      autoplaySwitch &&
+      currentTime !== 0 &&
+      duration !== 0 &&
+      currentTime === duration
+    ) {
       setWaitNotification(true);
       const timeoutId = setTimeout(() => {
         autoPlay();
@@ -118,6 +124,19 @@ function Video({ video, autoPlay }) {
           <div>{formatTime(duration)}</div>
         </div>
         <div className="mx-4 my-1 flex justify-end">
+          <label className="inline-flex items-center cursor-pointer px-4">
+            <span className="px-2">Auto Play:</span>
+            <input
+              type="checkbox"
+              value={autoplaySwitch}
+              checked={autoplaySwitch}
+              onChange={() => {
+                setAutoplaySwitch(!autoplaySwitch);
+              }}
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
           <label>
             Playback Speed:
             <select
